3a7b87ce3bb6c79997183249cd6f8c2e60776106
Racz <rtmate5@gmail.com>
2025-02-24 16:05:44 +0100

    valtozok bevezetese

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const lastnameHeaderLabel = 'Szerző neve'; // a szerzo nevet tartalmazo fejlec felirat
+const periodHeaderLabel = 'Korszak'; // a korszakot tartalmazo fejlec felirat
+const lovesHeaderLabel = 'Szerelmek'; // a szerelmeket tartalmazo fejlec felirat
+
+const firstRowAuthorValue = 'Balassi Bálint'; // az elso sor szerzojenek neve
+const firstRowPeriodValue = 'reformáció'; // az elso sor korszaka
+const firstRowLove1Value = 'Losonczy Anna'; // az elso sor elso szerelme
+const firstRowLove2Value = 'Dobó Krisztina'; // az elso sor masodik szerelme
+
+const secondRowAuthorValue = 'Csokonai Vitéz Mihály'; // a masodik sor szerzojenek neve
+const secondRowPeriodValue = 'felvilágosodás'; // a masodik sor korszaka
+const secondRowLoveValue = 'Vajda Juliána'; // a masodik sor szerelme
+
+const thirdRowAuthorValue = 'Petőfi Sándor'; // a harmadik sor szerzojenek neve
+const thirdRowPeriodValue = 'magyar romantika'; // a harmadik sor korszaka
+const thirdRowLove1Value = 'Mednyánszky Berta'; // a harmadik sor elso szerelme
+const thirdRowLove2Value = 'Szendrey Júlia'; // a harmadik sor masodik szerelme
+
+const fourthRowAuthorValue = 'Ady Endre'; // a negyedik sor szerzojenek neve
+const fourthRowPeriodValue = '20. század'; // a negyedik sor korszaka
+const fourthRowLove1Value = 'Léda'; // a negyedik sor elso szerelme
+const fourthRowLove2Value = 'Csinszka'; // a negyedik sor masodik szerelme
+
+
 const table = document.createElement('table'); // table elem letrehozasa
 document.body.appendChild(table); // hozzafuzes a bodyhoz
 
@@ -11,17 +35,17 @@ fejlec.appendChild(fejlecSor); // a sor hozzafuzese a fejlechez
 
 
 const fejlecCella1 = document.createElement('th'); // th elem letrehozasa a fejlec cellajahoz
-fejlecCella1.innerHTML = 'Szerzo neve'; // cell tartalma
+fejlecCella1.innerHTML = lastnameHeaderLabel; // cell tartalma const
 fejlecSor.appendChild(fejlecCella1); // cella hozzafuzese a fejlec sorhoz
 
 
 const fejlecCella2 = document.createElement('th'); // megegy th elem letrehozasa fejlec masodik cellajahoz
-fejlecCella2.innerHTML = 'Korszak'; // cella tartalma
+fejlecCella2.innerHTML = periodHeaderLabel; // cella tartalma const
 fejlecSor.appendChild(fejlecCella2); // cella hozzafuzese a fejlec sorhoz
 
 
 const fejlecCella3 = document.createElement('th'); // Letrehozok egy harmadik th elemet a fejlec harmadik cellajahoz
-fejlecCella3.innerHTML = 'Szerelmek'; // cella tartalma
+fejlecCella3.innerHTML = lovesHeaderLabel; // cella tartalma const
 fejlecCella3.colSpan = '2'; // 2 oszlopot foglaljon
 fejlecSor.appendChild(fejlecCella3); // cella hozzafuzese a fejlec sorhoz
 
@@ -35,22 +59,22 @@ torzs.appendChild(sor1); // az elso sort hozzafuzom a torzshoz
 
 
 const sor1Cella1 = document.createElement('td'); // td elem letrehozasa elso sor elso cellajahoz
-sor1Cella1.innerHTML = 'Balassi Balint'; // cella tartalma
+sor1Cella1.innerHTML = firstRowAuthorValue; // cella tartalma const
 sor1.appendChild(sor1Cella1); // cella hozzafuzese a sorhoz
 
 
 const sor1Cella2 = document.createElement('td'); // td elem letrehozasa elso sor masodik cellajahoz
-sor1Cella2.innerHTML = 'reformacio'; // cella tartalma
+sor1Cella2.innerHTML = firstRowPeriodValue; // cella tartalma const
 sor1.appendChild(sor1Cella2); // cella hozzafuzese a sorhoz
 
 
 const sor1Cella3 = document.createElement('td'); // td elem letrehozasa elso sor harmadik cellajahoz
-sor1Cella3.innerHTML = 'Losonczy Anna'; // cella tartalma
+sor1Cella3.innerHTML = firstRowLove1Value; // cella tartalma const 
 sor1.appendChild(sor1Cella3); // cella hozzafuzese a sorhoz
 
 
 const sor1Cella4 = document.createElement('td'); // td elem letrehozasa elso sor negyedik cellajahoz
-sor1Cella4.innerHTML = 'Dobo Krisztina'; // cella tartalma
+sor1Cella4.innerHTML = firstRowLove2Value; // cella tartalma const
 sor1.appendChild(sor1Cella4); // cella hozzafuzese a sorhoz
 
 
@@ -59,17 +83,17 @@ torzs.appendChild(sor2); // masodik sort hozzafuzom a torzshoz
 
 
 const sor2Cella1 = document.createElement('td'); // td elem letrehoz masodik sor elso cellajahoz
-sor2Cella1.innerHTML = 'Csokonai Vitez Mihaly'; // cella tartalma
+sor2Cella1.innerHTML = secondRowAuthorValue; // cella tartalma const
 sor2.appendChild(sor2Cella1); // cella hozzafuzese a torzshoz
 
 
 const sor2Cella2 = document.createElement('td'); // Letrehozok egy td elemet a masodik sor masodik cellajahoz
-sor2Cella2.innerHTML = 'felvilagosodas'; // cella tartalma
+sor2Cella2.innerHTML = secondRowPeriodValue; // cella tartalma const
 sor2.appendChild(sor2Cella2); // cella hozzafuzese a torzshoz
 
 
 const sor2Cella3 = document.createElement('td'); // Letrehozok egy td elemet a masodik sor harmadik cellajahoz
-sor2Cella3.innerHTML = 'Vajda Julianna'; // cella tartalma
+sor2Cella3.innerHTML = secondRowLoveValue; // cella tartalma const
 sor2Cella3.colSpan = '2'; // 2 oszlopot foglaljon
 sor2.appendChild(sor2Cella3); // cella hozzafuzese a torzshoz
 
@@ -79,22 +103,22 @@ torzs.appendChild(sor3); // harmadik sort hozzafuzom a torzshioz
 
 
 const sor3Cella1 = document.createElement('td'); // td elem letrehozasa a harmadik sor elso cellahoz
-sor3Cella1.innerHTML = 'Petofi Sandor'; // cella tartalma
+sor3Cella1.innerHTML = thirdRowAuthorValue; // cella tartalma const
 sor3.appendChild(sor3Cella1); // cella hozzafuzese a sorhoz
 
 
 const sor3Cella2 = document.createElement('td'); // td elem letrehozasa a harmadik sor masodik cellahoz
-sor3Cella2.innerHTML = 'magyar romantika'; // cella tartalma
+sor3Cella2.innerHTML = thirdRowPeriodValue; // cella tartalma const
 sor3.appendChild(sor3Cella2); // cella hozzafuzese a sorhoz
 
 
 const sor3Cella3 = document.createElement('td'); // td elem letrehozasa a harmadik sor harmadik cellahoz
-sor3Cella3.innerHTML = 'Mednyanszky Berta'; // cella tartalma
+sor3Cella3.innerHTML = thirdRowLove1Value; // cella tartalma const
 sor3.appendChild(sor3Cella3); // cella hozzafuzese a sorhoz
 
 
 const sor3Cella4 = document.createElement('td'); // td elem letrehozasa a harmadik sor negyedik cellahoz
-sor3Cella4.innerHTML = 'Szendrey Julia'; // cella tartalma
+sor3Cella4.innerHTML = thirdRowLove2Value; // cella tartalma const
 sor3.appendChild(sor3Cella4); // cella hozzafuzese a sorhoz
 
 
@@ -103,20 +127,20 @@ torzs.appendChild(sor4); // a negyedik sort hozzafuzom a torzshoz
 
 
 const sor4Cella1 = document.createElement('td'); // td elem letrehozasa a negyedik sor elso cellahoz
-sor4Cella1.innerHTML = 'Ady Endre'; // cella tartalma
+sor4Cella1.innerHTML = fourthRowAuthorValue; // cella tartalma const
 sor4.appendChild(sor4Cella1); // cellat hozzafuzom a sorhoz
 
 
 const sor4Cella2 = document.createElement('td'); // td elem letrehozasa a negyedik sor masodik cellahoz
-sor4Cella2.innerHTML = '20. szazad'; //cella tartalma
+sor4Cella2.innerHTML = fourthRowPeriodValue; //cella tartalma const
 sor4.appendChild(sor4Cella2); // cellat hozzafuzom a sorhoz
 
 
 const sor4Cella3 = document.createElement('td'); // td elem letrehozasa a negyedik sor harmadik cellahoz
-sor4Cella3.innerHTML = 'Leda'; // cella tartalma
+sor4Cella3.innerHTML = fourthRowLove1Value; // cella tartalma const
 sor4.appendChild(sor4Cella3); // cellat hozzafuzom a sorhoz
 
 
 const sor4Cella4 = document.createElement('td'); // td elem letrehozasa a negyedik sor negyedik cellahoz
-sor4Cella4.innerHTML = 'Csinszka'; // cella tartalma
+sor4Cella4.innerHTML = fourthRowLove2Value; // cella tartalma const
 sor4.appendChild(sor4Cella4); // cellat hozzafuzom a sorhoz
