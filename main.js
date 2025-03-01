c07b841620cc3cda788ed1c77f70fb39f404631b
kutyagel <rtmate5@gmail.com>
2025-03-01 19:43:53 +0100

    egyszeru validacio vegrehajtasa linearisan

--- main.js
+++ main.js
@@ -94,6 +94,7 @@ function renderMenu() {
 renderMenu(); // kezdeti render
 
 const form = document.getElementById('form'); // form elem lekerese ami a form idval van definialva
+
 form.addEventListener('submit', function(e) { // esemenykezeles a formra
     e.preventDefault(); // alapertelmezett esemeny megakadalyozasa
 
@@ -103,19 +104,55 @@ form.addEventListener('submit', function(e) { // esemenykezeles a formra
     const szerelem2Elem = document.getElementById('szerelem2'); // szerelem2 input elem lekerese
     const masodikElem = document.getElementById('masodik'); // masodik checkbox elem lekerese
 
+    const thisForm = e.currentTarget; // az aktualis form elmentese lokalis valtozoba
+    const errorHtmlElements = thisForm.querySelectorAll('.error'); // az osszes error osztalyu elem kivalasztasa a formbol
+    
+    for (const errorElement of errorHtmlElements) { // vegigmegy az osszes error elemen
+        errorElement.innerHTML = ''; // az errorElement tartalmat uresre allitja
+    }
+    
+    let valid = true; // validacios valtozo kezdeti ertekre allitasa
+
     const koltoNevValue = koltoNevElem.value; // kolto nev ertekenek kiolvasasa
     const korszakValue = korszakElem.value; // korszak ertekenek kiolvasasa
     const szerelem1Value = szerelem1Elem.value; // szerelem1 ertekenek kiolvasasa
     const szerelem2Value = masodikElem.checked ? szerelem2Elem.value : undefined; // szerelem2 ertekenek kiolvasasa ha be van pipalva
 
-    const newElement = { // uj elem letrehozasa a tombhoz
-        szerzo: koltoNevValue, // szerzo tulajdonsag beallitasaz
-        korszak: korszakValue, // korszak tulajdonsag beallitasaz
-        szerelem1: szerelem1Value, // szerelem1 tulajdonsag beallitasa
-        szerelem2: szerelem2Value // szerelem2 tulajdonsag beallitas
-    };
-    array.push(newElement); // uj elem hozzaadasa a tombhoz
-    menuContainer.innerHTML = ''; // tablazat tartalmanak torlese 
-    renderMenu(); // tablazat ujrarajzolasa
-    form.reset(); // form resetelese
+    if (koltoNevValue === '') { // ellenorzes, hogy a kolto neve ures-e
+        const koltoNevError = document.getElementById('kolto_nev-error'); // hibauzenet helyet tartalmazo elem lekerese
+        koltoNevError.innerHTML = 'A költő nevének megadása kötelező'; // hibauzenet beallitasa
+        valid = false; // validacios valtozo hamisra allitasa
+    }
+    
+    if (korszakValue === '') { // ha a korszak beviteli mezoje ures
+        const korszakError = document.getElementById('korszak-error'); // hibauzenet helyet tartalmazo elem lekerese
+        korszakError.innerHTML = 'A korszak megadása kötelező'; // hibauzenet beallitasa
+        valid = false; // validacios valtozo hamisra allitasa
+    }
+    
+    if (szerelem1Value === '') { // ha a szerelem1 beviteli mezoje ures
+        const szerelem1Error = document.getElementById('szerelem1-error'); // hibauzenet helyet tartalmazo elem lekerese
+        szerelem1Error.innerHTML = 'Az első szerelem megadása kötelező'; // hibauzenet beallitasa
+        valid = false; // validacios valtozo hamisra allitasa
+    }
+    
+    if (masodikElem.checked && (!szerelem2Value || szerelem2Value === '')) { // ellenorzes hogy a checkbox be van-e jelolve es a szerelem2 ures-e
+        const szerelem2Error = document.getElementById('szerelem2-error'); // hibauzenet helyet tartalmazo elem lekerese
+        szerelem2Error.innerHTML = 'A második szerelem megadása kötelező, ha be van jelölve'; // hibauzenet beallitasa
+        valid = false; // validacios valtozo hamisra allitasa
+    }
+    
+    if (valid) { // ha minden mezo helyesen van kitoltve
+        const newElem = { // definialok egy uj elementet
+            szerzo: koltoNevValue, // az uj objektum szerzo erteke a koltoNevValue lesz
+            korszak: korszakValue, // az uj objektum korszak erteke a korszakValue lesz
+            szerelem1: szerelem1Value, // az uj objektum szerelem1 erteke a szerelem1Value lesz
+            szerelem2: szerelem2Value // az uj objektum szerelem2 erteke a szerelem2Value lesz
+        };
+
+        array.push(newElem); // uj elem hozzaadasa a tombhoz
+        menuContainer.innerHTML = ''; // tablazat tartalmanak torlese 
+        renderMenu(); // tablazat ujrarajzolasa
+        thisForm.reset(); // form resetelese
+    }
 });
